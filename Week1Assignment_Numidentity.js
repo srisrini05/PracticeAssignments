3aa1e9467efdf3fa19f7789627475a2ac510d2a2
srisrini05 <srividhyasrinivasan79@gmail.com>
2026-04-28 18:58:46 -0400

    Add files via upload
    Week#1 Assignments

---
+++ Week1Assignment_Numidentity.js
@@ -0,0 +1,15 @@
+function numIdentity(params) {
+    if (params > 0) {
+        console.log("The given input is a positive number");
+        return false;
+    }
+    if (params < 0) {
+      console.log("The given input is a negative number");
+      return true;
+    } else { 
+        console.log("The given input is zero");
+    }
+   
+    }  
+
+numIdentity(-5);     
---
+++ Week1Assignment_OddNumber.js
@@ -0,0 +1,13 @@
+
+function oddNumber () {
+    for (let i = 1; i <= 25; i ++) 
+        {
+        if (i % 2 !== 0) 
+            {console.log(i);
+}
+    }
+}
+    oddNumber();
+
+
+
